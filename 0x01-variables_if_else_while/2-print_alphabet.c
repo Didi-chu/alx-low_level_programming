6129e741e4bff2847fd7eb92a70f48c41f799c2e
Didi-chu <endurancechukwu12@gmail.com>
2023-06-19 13:06:49 -0700

    commit all tasks

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Prints alphabet in lowercase
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++
+		}
+	putchar('\n');
+	return (0);
