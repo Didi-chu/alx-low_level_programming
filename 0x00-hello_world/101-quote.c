be337d3eecb4bc3684cba014c36ac5cdb771c1c4
Didi-chu <endurancechukwu12@gmail.com>
2023-06-19 16:44:38 -0700

    commit task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
- * main - A C program that prints exactly a content,  followed by a new line, to the standard error.
+ * main - A C program that prints an exact content
  * Return: Always 1.
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
