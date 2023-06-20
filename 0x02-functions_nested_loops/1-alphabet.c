18d2c5f5219890f33107cdcd63c195a13cf4d4f7
Didi-chu <endurancechukwu12@gmail.com>
2023-06-20 13:40:43 -0700

    commit task 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,13 @@
 #include "main.h"
 /**
- * main -  prints the alphabet, in lowercase
+ * print_alphabet -  prints the alphabet, in lowercase
  * Return: Always 0 (Success)
  */
-int main(void)
+void print_alphabet(void)
 {
-	int a;
+	char a;
 
 	for (a = 97; a <= 122; a++)
-	{
 		_putchar(a);
-	}
 	_putchar('\n');
 }
